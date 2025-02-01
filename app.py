3e4b59fc94ce8d4ff23a8022a923c76bd1cb456f
Ethan Hu <86241687+ehu9480@users.noreply.github.com>
2025-02-01 18:07:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -685,7 +685,7 @@ def serve_disk_video(filename):
     if '..' in filename or filename.startswith('/'):
         return "Invalid filename", 400
     
-    full_path = os.path.join("C:\\users\ataki\OneDrive\Documents", filename)
+    full_path = os.path.join("C:\sp25 auditions", filename)
     if not os.path.exists(full_path):
         return "File not found", 404
     
